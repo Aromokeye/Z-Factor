f056482dc76457b437df888491a612beaa0e8f28
Aromokeye <oyekeye@hotmail.com>
2020-12-16 23:28:38 +0100

    Updated about files and main.js processes

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ const {app, BrowserWindow, Menu, globalShortcut, ipcMain, shell} = require('elec
 const path = require('path')
 
 //environment settings
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'production'
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
+isWindows = process.platform === 'win32' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
         title: 'Z-Factor || Gas Compressibility Factor',
-        width: !isDev ? 800 : 550,
+        width: isDev ? 800 : 550,
         height: 600,
         icon: `${__dirname}./Assets/icons/icon.png`,
         resizable: isDev,
@@ -21,26 +23,72 @@ function createMainWindow() {
             nodeIntegration: true
           }
     })
-    if (isDev) {
-        mainWindow.webContents.openDevTools()
-      }
-      mainWindow.loadFile('./App/index.html')
+    mainWindow.loadFile('./App/index.html')
+
+    if(isDev){
+      mainWindow.webContents.openDevTools()
+    }
+}
+
+function createAboutWindow (){
+  aboutWindow = new BrowserWindow({
+      title: 'About Image Shrink',
+      width: 300,
+      height: 300,
+      resizable: false,
+      icon: `${__dirname}./Assets/icons/Icon.png`,
+      webPreferences: {
+          contextIsolation: true,
+          nodeIntegration: true
+        }
+  })
+
+  //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  aboutWindow.loadFile('./App/about.html')
 }
 
 app.on('ready', ()=>{
     createMainWindow()
+    //createAboutWindow()
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
+
+    globalShortcut.register('CmdOrCtrl+R', ()=> mainWindow.reload())
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', 
+                            ()=> mainWindow.toggleDevTools())
+
+    mainWindow.on('closed', ()=>mainWindow = null)
 })
+
 
 
 
 //menu design
 const menu = [
-    ...(isMac ? [{ role: 'appMenu' }] : []),
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        {
+          label: 'About',
+          click: createAboutWindow
+        }
+      ]
+    }] : [] ),
+    
     {
       role: 'fileMenu',
     },
+    ...(isWindows && [
+      {
+        label: 'Help',
+        submenu:[
+          {
+            label: 'About',
+            click: createAboutWindow,
+          }
+        ]
+      }
+    ]),
     ...(isDev
       ? [
           {
